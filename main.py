5f259ff49c75e540fa5f1231c0eba3a993cb5056
973398769 <nini_good@outlook.com>
2024-09-14 13:18:20 +0000

    update

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ HOME = os.getcwd()
 sys.path.insert(0, "weights")
 sys.path.insert(0, "weights/GroundingDINO")
 sys.path.insert(0, "weights/segment-anything")
-os.chdir("/src/weights/GroundingDINO")
-subprocess.call([sys.executable, '-m', 'pip', 'install', '-e', '.'], env=env_vars)
-os.chdir("/src/weights/segment-anything")
-subprocess.call([sys.executable, '-m', 'pip', 'install', '-e', '.'], env=env_vars)
+os.chdir("/home/ni/prop/grounded_sam_replicate2/weights/GroundingDINO")
+#subprocess.call([sys.executable, '-m', 'pip', 'install', '-e', '.'], env=env_vars)
+os.chdir("/home/ni/prop/grounded_sam_replicate2/weights/segment-anything")
+#subprocess.call([sys.executable, '-m', 'pip', 'install', '-e', '.'], env=env_vars)
 os.chdir(HOME)
 
 import torch
@@ -22,52 +22,73 @@ from segment_anything import build_sam, SamPredictor
 from grounded_sam import run_grounding_sam
 # import uuid
 from hf_path_exports import cache_config_file, cache_file
-class Predictor:
-    def setup(self):
-        """Load the model into memory to make running multiple predictions efficient"""
-        print("Loading pipelines...x")
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        def load_model_hf(device='cpu'):
-            args = SLConfig.fromfile(cache_config_file)
-            args.device = device
-            model = build_model(args)
-            checkpoint = torch.load(cache_file, map_location=device)
-            log = model.load_state_dict(clean_state_dict(checkpoint['model']), strict=False)
-            print("Model loaded from {} \n => {}".format(cache_file, log))
-            _ = model.eval()
-            return model
-
-        self.groundingdino_model = load_model_hf(device)
-        sam_checkpoint = '/src/weights/sam_vit_h_4b8939.pth'
-        self.sam_predictor = SamPredictor(build_sam(checkpoint=sam_checkpoint).to(device))
-
-    def predict(image, mask_prompt, negative_mask_prompt, adjustment_factor):
-        # predict_id = str(uuid.uuid4())
-
-        # print(f"Running prediction: {predict_id}...")
-
-        annotated_picture_mask, neg_annotated_picture_mask, mask, inverted_mask = run_grounding_sam(image,
-                                                                                                    mask_prompt,
-                                                                                                    negative_mask_prompt,
-                                                                                                    self.groundingdino_model,
-                                                                                                    self.sam_predictor,
-                                                                                                    adjustment_factor)
-
-        print("Done!")
 
 
+def setup():
+    """Load the model into memory to make running multiple predictions efficient"""
+    print("Loading pipelines...")
 
-predictor = Predictor()
+    # 选择设备：GPU 或 CPU
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# 调用 setup 方法加载模型
-predictor.setup()
+    # 定义加载模型的函数
+    def load_model_hf(device='cpu'):
+        args = SLConfig.fromfile(cache_config_file)
+        args.device = device
+        model = build_model(args)
+        checkpoint = torch.load(cache_file, map_location=device)
+        log = model.load_state_dict(clean_state_dict(checkpoint['model']), strict=False)
+        print(f"Model loaded from {cache_file} \n => {log}")
+        _ = model.eval()
+        return model
+
+    # 加载 groundingdino 模型
+    groundingdino_model = load_model_hf(device)
+
+    # 加载 SAM 模型
+    sam_checkpoint = '/home/ni/prop/grounded_sam_replicate2/weights/sam_vit_h_4b8939.pth'
+    sam_predictor = SamPredictor(build_sam(checkpoint=sam_checkpoint).to(device))
+
+    return groundingdino_model, sam_predictor
+
+
+def predict(groundingdino_model, sam_predictor, image, mask_prompt, negative_mask_prompt, adjustment_factor):
+    """运行预测"""
+    #predict_id = str(uuid.uuid4())
+    #print(f"Running prediction: {predict_id}...")
+
+    # 调用 groundingdino 和 sam 模型进行预测
+    annotated_picture_mask, neg_annotated_picture_mask, mask, inverted_mask = run_grounding_sam(
+        image, mask_prompt, negative_mask_prompt,
+        groundingdino_model, sam_predictor,
+        adjustment_factor
+    )
+    annotated_picture_mask.save("1.png")
+    neg_annotated_picture_mask.save("2.png")
+    mask.save("3.png")
+    inverted_mask.save("4.png")
+    print("Done!")
+    return annotated_picture_mask, neg_annotated_picture_mask, mask, inverted_mask
+
+
+# 使用函数
+groundingdino_model, sam_predictor = setup()
 
 # # 定义预测所需的输入参数
-# image = ...  # 你的图片数据（例如numpy array或其他格式）
-# mask_prompt = ...  # mask的提示
-# negative_mask_prompt = ...  # 负mask提示
-# adjustment_factor = 1.0  # 你希望的调整因子
+image = "/home/ni/prop/grounded_sam_replicate2/image_11057_original.jpeg"  # 你的图片数据（例如numpy array或其他格式）
+mask_prompt = "watermark, text"  # mask的提示
+negative_mask_prompt = ""  # 负mask提示
+adjustment_factor = 10  # 你希望的调整因子
 #
-# # 调用 predict 函数
-# predictor.predict(image, mask_prompt, negative_mask_prompt, adjustment_factor)
+# # 调用 predict 
+annotated_picture_mask, neg_annotated_picture_mask, mask, inverted_mask = predict(groundingdino_model, sam_predictor,image, mask_prompt, negative_mask_prompt, adjustment_factor)
+annotated_picture_mask.save("1.png")
+neg_annotated_picture_mask.save("2.png")
+mask.save("3.png")
+inverted_mask.save("4.png")
+image = "/home/ni/prop/grounded_sam_replicate2/image_01790_original.jpeg"
+annotated_picture_mask, neg_annotated_picture_mask, mask, inverted_mask = predict(groundingdino_model, sam_predictor,image, mask_prompt, negative_mask_prompt, adjustment_factor)
+annotated_picture_mask.save("4.png")
+neg_annotated_picture_mask.save("5.png")
+mask.save("6.png")
+inverted_mask.save("7.png")
